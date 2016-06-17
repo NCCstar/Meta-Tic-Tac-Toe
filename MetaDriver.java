c94fb59da668e982c95c7e50eedfb6822b38a2b3
NCCstar <rhyspmiller@gmail.com>
2016-06-17 09:56:02 -0400

    Pre-play interface

--- MetaDriver.java
+++ MetaDriver.java
@@ -3,7 +3,7 @@ public class MetaDriver
 {
    public static void main(String[] arg)
    {
-      /*
+      
       Object[] options = {"Regular","Meta-TTT","Meta^2"};//possible player numbers
       String sel = (String)JOptionPane.showInputDialog(null,"What type of Tic-Tac-Toe?","Choices",JOptionPane.INFORMATION_MESSAGE, null,options, options[0]);
       int layers=2;
@@ -19,8 +19,8 @@ public class MetaDriver
             layers=2;
             break;
       }
-      */
-      MetaBoard board = new MetaBoard(0);
+      
+      MetaBoard board = new MetaBoard(layers);
       JFrame frame = new JFrame("Meta-Tic-Tac-Toe");
       frame.setSize(1000,1000);
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
